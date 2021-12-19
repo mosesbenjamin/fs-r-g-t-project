6e3fb9c466c649de68c19a6ca35630bd8ac64e05
mosesbenjamin <mavewreek@gmail.com>
2021-12-19 16:15:21 +0100

    microORM setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("hello homie...")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroConfig);
+    await orm.getMigrator().up();
+
+    const generator = orm.getSchemaGenerator();
+    await generator.updateSchema();
+
+    // const post = orm.em.create(Post, {title: "my first post 2"});
+    // await orm.em.persistAndFlush(post);
+
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+}
+
+main().catch(err => {
+    console.error(err);
+})
+
+
+
+console.log("hello world...")
