66c62558a4a6e3801e921c8c4eb30ec534e896fa
mosesbenjamin <mavewreek@gmail.com>
2021-12-19 17:54:27 +0100

    mikroorm typegraphql crud

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType() // exposes to graphql schema using type-graphql
 @Entity()
 export class Post {
+  @Field() // exposes to graphql schema using type-graphql
   @PrimaryKey()
   _id!: number;
 
+  @Field()
   @Property()
   createdAt: Date = new Date();
 
+  @Field()
   @Property({ onUpdate: () => new Date() })
   updatedAt: Date = new Date();
   
+  @Field()
   @Property({type: 'text'})
   title!: string;
 }
