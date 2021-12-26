ea60a2235eced8540d3fbf35277c5186814e8f28
mosesbenjamin <mavewreek@gmail.com>
2021-12-26 21:21:18 +0100

    implement logout

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,6 +2,7 @@ import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from 'argon2'
+import { COOKIE_NAME } from '../constants'
 
 @InputType()
 class UsernamePasswordInput {
@@ -122,5 +123,20 @@ export class UserResolver {
         return {
             user
         };
+    }
+
+    @Mutation(() => Boolean)
+    logout(
+        @Ctx() {req, res} : MyContext
+    ) {
+        return new Promise(resolve => req.session.destroy(err => {
+            res.clearCookie(COOKIE_NAME)
+            if(err) {
+                console.error(err)
+                resolve(false)
+                return
+            }
+            resolve(true)
+        }))
     }
 }
