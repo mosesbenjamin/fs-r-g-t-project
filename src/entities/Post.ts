8134611e92265c1efb82c4cbfa97742271597251
mosesbenjamin <mavewreek@gmail.com>
2022-01-02 21:20:13 +0100

    many to one

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from "type-graphql";
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./User";
 
 @ObjectType() // exposes to graphql schema using type-graphql
 @Entity()
@@ -7,6 +8,25 @@ export class Post extends BaseEntity{
   @Field() // exposes to graphql schema using type-graphql
   @PrimaryGeneratedColumn()
   id!: number;
+  
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: 'int', default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  creator: User;
 
   @Field()
   @CreateDateColumn()
@@ -15,8 +35,4 @@ export class Post extends BaseEntity{
   @Field()
   @UpdateDateColumn()
   updatedAt: Date
-  
-  @Field()
-  @Column()
-  title!: string;
 }
